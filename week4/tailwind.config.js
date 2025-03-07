d68a868378c1289d5606c885e5640946dbaca3a2
Farah Faizah <farahfaizah275@gmail.com>
2025-03-07 22:46:05 +0700

    hero section commit

--- week4/tailwind.config.js
+++ week4/tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         primary : '#c2410c',
         secondary : '#164e63'
-      }
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
     },
   },
   plugins: [],
